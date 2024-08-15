1481c74ef3b3fc7b5a791b824ef8e8a9fa624d39
adhi0987 <adhithyashivakumar2511@gmail.com>
2024-08-15 19:33:23 +0530

    Added Tokens.h

--- Group _Ass1/Test.c
+++ Group _Ass1/Test.c
@@ -1,1 +1,18 @@
-+-++--/%*
+#include <stdio.h>
+
+// Include the generated lexer file
+#include "lex.yy.c" 
+
+
+// Main function
+int main() {
+
+    int nextToken;
+
+    while(nextToken = yylex())
+    {
+        printf("%d => %s\n",nextToken,yytext);
+    }
+
+    return 0;
+}
